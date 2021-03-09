3c91218e440baddb59e93423e430958ce6ae4e86
JGrunklee <grunkles16@gmail.com>
2021-03-08 23:49:27 -0600

    create firmware project

---
+++ GrunkDeskClock.X/main.c
@@ -0,0 +1,21 @@
+/* 
+ * GrunkDeskClock
+ * 
+ * File:   main.c
+ * Author: Jacob Grunklee
+ * Comments: Entry point and top-level coordination
+ * Revision history: 
+ */
+
+
+#include "main.h"
+
+int main(void) {
+    
+    while(1) {
+        asm("nop"); //\todo
+        asm("nop"); //\todo
+        asm("nop"); //\todo
+    }
+    return 0;
+}
---
+++ GrunkDeskClock.X/main.h
@@ -0,0 +1,41 @@
+/* 
+ * GrunkDeskClock
+ * 
+ * File:   main.h
+ * Author: Jacob Grunklee
+ * Comments: Configuration bits and top-level includes for GDC
+ * Revision history: 
+ */
+
+// PIC24FJ32GA004 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG2
+#pragma config POSCMOD = NONE           // Primary Oscillator Select (Primary oscillator disabled)
+#pragma config I2C1SEL = PRI            // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
+#pragma config IOL1WAY = OFF            // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
+#pragma config OSCIOFNC = ON            // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as port I/O (RC15))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = FRCDIV           // Oscillator Select (Fast RC Oscillator with Postscaler (FRCDIV))
+#pragma config SOSCSEL = SOSC           // Sec Oscillator Select (Default Secondary Oscillator (SOSC))
+#pragma config WUTSEL = FST             // Wake-up timer Select (Fast Wake-up Timer)
+#pragma config IESO = ON                // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) enabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (Watchdog Timer is enabled)
+#pragma config ICS = PGx1               // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+
+
